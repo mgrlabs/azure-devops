9e596eb415f2d057315d4fdb5ab114804c92a81a
Mark Revill <mrevill@outlook.com>
2019-07-10 07:58:13 +0100

    Update Agent-UseSystemAccessTokenForGit.ps1

--- Agent-UseSystemAccessTokenForGit.ps1
+++ Agent-UseSystemAccessTokenForGit.ps1
@@ -1,7 +1,23 @@
-$Auth = [convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes(":$Env:SYSTEM_ACCESSTOKEN"))
-$azDevopsOrg = "vibrato-mark"
-$azDevopsProject = "New%20Signature"
+if ("$(Build.BuildNumber)" -notlike "*Release*") 
+{
+    # Take the agent OAuth token and convert it to base64
+    $Auth = [convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes(":$Env:SYSTEM_ACCESSTOKEN"))
 
-$RepoUrl = "https://$azDevopsOrg@dev.azure.com/$azDevopsOrg/$azDevopsProject/_git/$(Build.Repository.Name)"
+    # Clone the Azure Repo using the basic auth
+    git -c http.$(Release.Artifacts.GitSource.Repository.Uri).extraheader="AUTHORIZATION: basic $Auth" `
+        clone "$(Release.Artifacts.GitSource.Repository.Uri)"
 
-git -c http.$RepoUrl.extraheader="AUTHORIZATION: basic $Auth" clone $RepoUrl
+    cd $(Build.Repository.Name)
+
+    # Configure git with the context of the user who initiated the pipeline, checkout the commit and tag then push the tag
+    git config --global user.email "$(Release.RequestedForEmail)"
+    git config --global user.name "$(Release.RequestedFor)"
+    git checkout $(Release.Artifacts.BuildSource.SourceVersion)
+    git tag -a $(Build.BuildNumber) -m $(Build.BuildNumber)
+    git -c http.$(Release.Artifacts.GitSource.Repository.Uri).extraheader="AUTHORIZATION: basic $Auth" push origin --tag
+
+    # Update the build number relating  to this to mark for release
+    Write-Host "##vso[Build.UpdateBuildNumber]$(Build.BuildNumber)-Release"
+} else {
+    Write-Host "Build already tagged for release..."
+}
